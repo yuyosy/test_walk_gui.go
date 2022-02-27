2a26b9d1756d88a8a0467373d87b4537928665bb
yuyosy <y-ysss@outlook.com>
2022-02-27 20:00:37 +0900

    Add: logview.go

---
+++ logview.go
@@ -0,0 +1,79 @@
+//
+// https://github.com/lxn/walk/tree/master/examples/logview
+//
+
+//
+// Copyright 2012 The Walk Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+//
+// This code was partially written by xlplbo (https://github.com/xlplbo)
+// https://github.com/lxn/walk/pull/657
+//
+
+package main
+
+import (
+	"github.com/lxn/walk"
+	"github.com/lxn/win"
+)
+
+type LogView struct {
+	walk.TextEdit
+	logChan chan string
+	maxSize int
+}
+
+const (
+	TEM_APPENDTEXT = win.WM_USER + 6
+)
+
+func NewLogView(parent walk.Container) (*LogView, error) {
+	lv := &LogView{
+		logChan: make(chan string, 1024),
+		maxSize: 0x7FFF0000,
+	}
+	if err := walk.InitWidget(lv,
+		parent,
+		"EDIT",
+		win.WS_TABSTOP|win.WS_VISIBLE|win.WS_VSCROLL|win.ES_MULTILINE|win.ES_WANTRETURN,
+		win.WS_EX_CLIENTEDGE); err != nil {
+		return nil, err
+	}
+	lv.TextEdit.SetReadOnly(true)
+	lv.TextEdit.SetMaxLength(lv.maxSize)
+	return lv, nil
+}
+
+func (lv *LogView) AppendText(value string) {
+	textLength := lv.TextEdit.TextLength()
+	if textLength+len(value) < lv.maxSize {
+		lv.TextEdit.AppendText(value)
+	} else {
+		lv.TextEdit.SetText(value)
+	}
+}
+
+func (lv *LogView) PostAppendText(value string) {
+	lv.logChan <- value
+	win.PostMessage(lv.Handle(), TEM_APPENDTEXT, 0, 0)
+}
+
+func (lv *LogView) Write(p []byte) (int, error) {
+	lv.PostAppendText(string(p) + "\r\n")
+	return len(p), nil
+}
+
+func (lv *LogView) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	switch msg {
+	case TEM_APPENDTEXT:
+		select {
+		case value := <-lv.logChan:
+			lv.AppendText(value)
+		default:
+		}
+	}
+	return lv.TextEdit.WndProc(hwnd, msg, wParam, lParam)
+}
